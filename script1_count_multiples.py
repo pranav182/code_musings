0d2a878534877f93914b294da035040aa7eac8c9
Pranav Shetty <pranavchess@gmail.com>
2022-12-03 09:59:01 +0530

    efficient method added

--- script1_count_multiples.py
+++ script1_count_multiples.py
@@ -1,4 +1,6 @@
-# Find total number of multiples of a given number (number) between two numbers (k and n)
+# Find total number of multiples of a given number (number) between two numbers (k and n) inclusive of both numbers (k and n).
+import math
+
 k = 21
 n = 200
 number = [i for i in range(1, 101)] # Took all numbers between 1 and 100 for testing
@@ -8,3 +10,8 @@ for ele in number:
     else:
         ans = ((n//ele) - (k//ele)) # Formulae when k is not divisible by number
     print(f"k = {k}, n = {n}, number = {ele}, answer = {ans}")
+    
+    # Efficient method found!!
+    ANS = (n//ele - math.ceil(k/ele)) + 1
+    print(ANS)
+    print('')
